55c68e44bc9b77fed0f7fe87dcf7d0b5917ccf23
Bereket Nigussie <bereketnigussie9@gmail.com>
2023-08-16 12:41:32 +0300

    final

--- middlewares/owner.js
+++ middlewares/owner.js
@@ -3,9 +3,18 @@ module.exports = async function (req, res, next) {
     const {id}= req.params
   console.log(req.user, req.params)
   try {
-    const tasks = await Task.findById(id)
-    console.log(tasks, user)
-    
+    const projectId = req.params.id; 
+    const userId = req.user._id; 
+
+    const project = await Project.findById(projectId);
+
+    if (!project) {
+      return res.status(404).json({ error: 'Project not found' });
+    }
+
+    if (project.owner.toString() !== userId) {
+      return res.status(403).json({ error: 'Access denied. You are not the owner of this project' });
+    }
     next();
   }
   catch (ex) {
